e476c6e1e1e9af40970d63be6c176687530a65e6
create-xiaoyu <abc18873060306@qq.com>
2026-06-09 23:55:14 +0800

    remake

--- src/main/java/com/xiaoyu/minigame/gamefeature/chunkplaceblock/placement/ChunkPlaceOperation.java
+++
@@ -1,7 +0,0 @@
-package com.xiaoyu.minigame.gamefeature.chunkplaceblock.placement;
-
-public enum ChunkPlaceOperation {
-    PLACE_IN_EMPTY_BLOCKS,
-    SYNC_BLOCK_ENTITY_DATA,
-    DESTROY_SAME_POSITION_BLOCKS
-}
